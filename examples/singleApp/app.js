0878a74aa64e546b904679957a2424e1fa22df38
Andrei Kovrov <a@devs.team>
2024-06-15 00:58:15 +0200

    feat: add schema ref for body tag

--- examples/singleApp/app.js
+++ examples/singleApp/app.js
@@ -87,5 +87,15 @@ app.head('/api/v1/song/:id/*', (_req, res) => (
   })
 ));
 
+/**
+ * @description Updates a new song
+ * @body #/definitions/Song
+ */
+app.post('/api/v3/song-json', (_req, res) => (
+  res.json({
+    title: 'abum 1',
+  })
+));
+
 
 module.exports = app;
