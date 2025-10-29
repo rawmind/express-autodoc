c939aa1a7d51f8bc33b5866334c592d2c1f9e73a
Andrei Kovrov <a@devs.team>
2025-10-29 04:29:55 +0100

    fix: parse AST only if the express keyword is found (#32)
    * fix: speedup AST parsing
    
    * fix: speedup AST parsing
    
    * fix: disable logging

--- src/traverse.js
+++ src/traverse.js
@@ -3,7 +3,7 @@ const { walk } = require('./walker')
 const { ExpressProject } = require('./swagger/ExpressProject')
 
 exports.generateSwagger = (projectDir, config, outputFile = 'swagger_output.json') => {
-  const modules = walk(projectDir).map(f => new JsModule(f, projectDir).traverseAst())
-  const expressModules = modules.filter(m => m.hasExpress())
+  const modules = walk(projectDir).map(f => new JsModule(f, projectDir))
+  const expressModules = modules.filter(m => m.hasExpress()).map(m => m.traverseAst())
   return new ExpressProject(expressModules).generateSwagger(config, outputFile)
 }
